cca125d5ef80a82bffe3e598d9e962fdc7b4a286
Andrew Gaskell <gskll@protonmail.com>
2024-11-18 15:07:28 +0100

    lenght of longest substring

---
+++ 3-longest-substring-without-repeating-characters/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Given a string s, find the length of the longest substring without repeating characters.
+
+func lengthOfLongestSubstring(s string) int {
+	length := 0
+	seen := make(map[byte]int)
+
+	for l, r := 0, 0; r < len(s); r++ {
+		char := s[r]
+		if i, ok := seen[char]; ok && l <= i {
+			l = i + 1
+		}
+
+		seen[char] = r
+		length = max(length, r-l+1)
+
+	}
+
+	return length
+}
+
+func main() {
+	type test struct {
+		in  string
+		out int
+	}
+	tests := []test{
+		{in: "abcabcbb", out: 3},
+		{in: "bbbbb", out: 1},
+		{in: "pwwkew", out: 3},
+		{in: "abba", out: 2},
+	}
+	for i, t := range tests {
+		res := lengthOfLongestSubstring(t.in)
+		fmt.Printf("%d: %+v --> %+v == %+v\n", i, t.in, res, t.out)
+	}
+}
