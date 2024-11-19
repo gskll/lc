3b290a1aa59e9526ee3cac043e7bd342b47bd6a0
Andrew Gaskell <gskll@protonmail.com>
2024-11-19 16:55:18 +0100

    reorg

--- 4-sliding-window/567-permutation-in-string/main.go
+++ 4-sliding-window/567-permutation-in-string/main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// https://leetcode.com/problems/permutation-in-string/description/
+
+// Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+// In other words, return true if one of s1's permutations is the substring of s2.
+
 func checkInclusion(s1 string, s2 string) bool {
 	if len(s1) > len(s2) {
 		return false
